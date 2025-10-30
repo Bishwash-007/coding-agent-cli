d9a3e212b54e740ab23f816b290d0aadbe605857
Bishwash <drewwbishwash@gmail.com>
2025-10-30 10:18:58 +0545

    added run python file

---
+++ functions/write_file.py
@@ -0,0 +1,29 @@
+import os
+
+def write_file(working_directory, file_path, content):
+    abs_working_directory = os.path.abspath(working_directory)        
+    abs_file_path = os.path.abspath(os.path.join(working_directory, file_path))
+    
+    if not abs_file_path.startswith(abs_working_directory):
+        return f"Error: {abs_file_path} is not in the working directory"
+    
+    parent_dir = os.path.dirname(abs_file_path)
+    if not os.path.isdir(parent_dir):
+        try:
+            os.makedirs(parent_dir)
+        except Exception as e:
+            return f"something went wrong while creating parent directory {e}"
+        
+    if not os.path.isfile(abs_file_path):
+        pass
+        
+    try:
+        with open(abs_file_path,"w") as f:
+            f.write(content)
+        return f"'{file_path}' Modified ... ({len(content)}) characters wrote to file"
+        
+    except Exception as e:
+        return f"Failed to write to file: {file_path}, {e}"
+    
+    
+    
--- test.py
+++ test.py
@@ -1,6 +1,18 @@
 from functions.get_files_info import get_file_info
+from functions.get_files_content import get_file_content
+from functions.write_file import write_file
+from functions.run_python_file import run_python_file
 
 def main():
+    working_directory = 'calculator'
+    print(run_python_file(working_directory, 'lorem.txt'))
+    print(run_python_file(working_directory, 'tests.py'))
+
+main()
+
+
+
+def test_get_file_info():
     working_directory = 'calculator'
     root_content = get_file_info(working_directory)
     print(f"Root Content : {root_content}")
@@ -11,4 +23,19 @@ def main():
     child_content = get_file_info(working_directory, '../bin')
     print(f"Child Content : {child_content}")
     
-main()
+    
+def test_get_file_content():
+    working_directory = 'calculator'
+    print(get_file_content(working_directory,'lorem.txt'))
+    print(get_file_content(working_directory,'main.py'))
+    print(get_file_content(working_directory,'tests.py'))
+    print(get_file_content(working_directory,'pkg/calculator.py'))
+    print(get_file_content(working_directory,'pkg/hello.py'))
+    
+    
+def test_write_file():
+    working_directory = 'calculator'
+    print(write_file(working_directory, 'lorem.txt', "This is the new code to be written in the file"))
+    print(write_file(working_directory, 'hello.js', "console.log('Hello World!')"))
+    print(write_file(working_directory, 'temp/test_case.js', "console.log('Test World!')"))
+    print(write_file(working_directory, 'pkg/case.js', "console.log('Test World!')"))
